72942908d45e91de935c257075c63e81d2286fb4
Ian Okumu <okumu.otsembo@gmail.com>
2022-05-05 10:32:33 +0300

    Completed Assignment

---
+++ app.rb
@@ -0,0 +1,8 @@
+# prints string and adds a blank line
+puts "Hello World!"
+# print non string like variables
+p [1,2,3]
+# pretty print
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
